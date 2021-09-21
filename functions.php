7c899f049747d2664f92cd64930516e561dc669b
StineAndersenHolm <sti-holm@hotmail.com>
2021-09-21 09:32:53 +0200

    ændring 3

--- functions.php
+++ functions.php
@@ -3,15 +3,15 @@
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
         
-    $parenthandle = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+    $parenthandle = 'parent-style'; // Definition af parent-style
     $theme = wp_get_theme();
-    wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css', 
-        array(),  // if the parent theme code has a dependency, copy it to here
+    wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css', //Her linker jeg til parent style.css
+        array(),  
         $theme->parent()->get('Version')
     );
-    wp_enqueue_style( 'child-style', get_stylesheet_uri(),
+    wp_enqueue_style( 'child-style', get_stylesheet_uri(), //Her linker jeg til child style.css
         array( $parenthandle ),
-        $theme->get('Version') // this only works if you have Version in the style header
+        $theme->get('Version') // Versionnummeret
     );
 }
 
